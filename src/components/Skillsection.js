a4122ccd0359f43f0c84cd15e459e7fb078f41e6
alex jenter <alex@attribute.ch>
2018-01-16 12:18:55 +0100

    changes to skill section content and styling

--- src/components/Skillsection.js
+++ src/components/Skillsection.js
@@ -4,22 +4,24 @@ import Skillitem from './Skillitem'
 export default () => (
   <section className="section section--skills">
     <h2>Skills</h2>
-    <div>
-      <h3>Design</h3>
-      <ul>
-        <Skillitem score="8" name="Sketch.app" />
-        <Skillitem score="9" name="Adobe CC" />
-      </ul>
-    </div>
-    <div>
-      <h3>Code</h3>
-      <ul>
-        <Skillitem score="9" name="HTML/CSS" />
-        <Skillitem score="8" name="Javascript" />
-        <Skillitem score="7" name="Twig" />
-        <Skillitem score="5" name="PHP" />
-        <Skillitem score="4" name="Python" />
-      </ul>
+    <div className="section__content">
+      <div>
+        <h3>Design</h3>
+        <ul>
+          <Skillitem score="8" name="Sketch.app" />
+          <Skillitem score="9" name="Adobe CC" />
+        </ul>
+      </div>
+      <div>
+        <h3>Code</h3>
+        <ul>
+          <Skillitem score="9" name="HTML/CSS" />
+          <Skillitem score="8" name="Javascript" />
+          <Skillitem score="7" name="Twig" />
+          <Skillitem score="2" name="PHP" />
+          <Skillitem score="3" name="Python" />
+        </ul>
+      </div>
     </div>
   </section>
 )
