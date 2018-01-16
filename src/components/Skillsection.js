f303368cedeb124c00844fd3893c1397701ea3f8
alex jenter <alex@attribute.ch>
2018-01-16 12:23:33 +0100

    skill section order and layout

--- src/components/Skillsection.js
+++ src/components/Skillsection.js
@@ -6,13 +6,6 @@ export default () => (
     <h2>Skills</h2>
     <div className="section__content">
       <div>
-        <h3>Design</h3>
-        <ul>
-          <Skillitem score="8" name="Sketch.app" />
-          <Skillitem score="9" name="Adobe CC" />
-        </ul>
-      </div>
-      <div>
         <h3>Code</h3>
         <ul>
           <Skillitem score="9" name="HTML/CSS" />
@@ -20,6 +13,13 @@ export default () => (
           <Skillitem score="7" name="Twig" />
           <Skillitem score="2" name="PHP" />
           <Skillitem score="3" name="Python" />
+        </ul>
+      </div>
+      <div>
+        <h3>Design</h3>
+        <ul>
+          <Skillitem score="8" name="Sketch.app" />
+          <Skillitem score="9" name="Adobe CC" />
         </ul>
       </div>
     </div>
