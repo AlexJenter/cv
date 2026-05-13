ef5909f565e944bb267c0d929636feb61aca4726
Alex Jenter <AlexJenter@users.noreply.github.com>
2026-05-13 11:38:50 +0200

    Project links tooltips

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Link from "gatsby-link";
 
-import Skillsection from "../components/Skillsection";
 import FirstSection from "../components/FirstSection";
-import Section from "../components/Section";
 
 import image from "../../static/foto-av.jpg";
 
@@ -24,32 +22,105 @@ const links = [
   { url: "https://versive.cc", label: "Versive" }
 ];
 
-const ProjectsPage = ({ data }) => (
-  <article>
-    <header>
-      <h1>
-        <Link to="/">Alex Jenter</Link>
-      </h1>
-      <img className="portrait" src={image} alt="portrait of alex jenter" />
-      {data.person.edges.map(x => (
-        <FirstSection key={x.node.id} {...x.node} page="projects" />
-      ))}
-    </header>
+const idleCallback =
+  typeof window !== "undefined" && window.requestIdleCallback
+    ? cb => window.requestIdleCallback(cb)
+    : cb => setTimeout(cb, 100);
 
-    <main>
-      <h2>Recent Projects</h2>
-      <ul>
-        {links.map(({ url, label }) => (
-          <li>
-            <a href={url} target="_blank" rel="noopener noreferrer">
-              {label}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </main>
-  </article>
-);
+class ProjectsPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      screenshots: {},
+      hoveredUrl: null,
+      tooltipPos: { x: 0, y: 0 }
+    };
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+  }
+
+  componentDidMount() {
+    links.forEach(({ url }) => {
+      idleCallback(() => {
+        fetch(
+          `https://api.microlink.io/?url=${encodeURIComponent(
+            url
+          )}&screenshot=true&meta=false`
+        )
+          .then(res => res.json())
+          .then(data => {
+            if (data.status === "success" && data.data.screenshot) {
+              this.setState(prev => ({
+                screenshots: {
+                  ...prev.screenshots,
+                  [url]: data.data.screenshot.url
+                }
+              }));
+            }
+          })
+          .catch(() => {});
+      });
+    });
+  }
+
+  handleMouseMove(e) {
+    if (this.state.hoveredUrl) {
+      this.setState({ tooltipPos: { x: e.clientX, y: e.clientY } });
+    }
+  }
+
+  render() {
+    const { data } = this.props;
+    const { screenshots, hoveredUrl, tooltipPos } = this.state;
+    const screenshotUrl = hoveredUrl && screenshots[hoveredUrl];
+
+    return (
+      <article onMouseMove={this.handleMouseMove}>
+        <header>
+          <h1>
+            <Link to="/">Alex Jenter</Link>
+          </h1>
+          <img className="portrait" src={image} alt="portrait of alex jenter" />
+          {data.person.edges.map(x => (
+            <FirstSection key={x.node.id} {...x.node} page="projects" />
+          ))}
+        </header>
+
+        <main>
+          <h2>Recent Projects</h2>
+          <ul>
+            {links.map(({ url, label }) => (
+              <li key={url}>
+                <a
+                  href={url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onMouseEnter={e =>
+                    this.setState({
+                      hoveredUrl: url,
+                      tooltipPos: { x: e.clientX, y: e.clientY }
+                    })
+                  }
+                  onMouseLeave={() => this.setState({ hoveredUrl: null })}
+                >
+                  {label}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </main>
+
+        {screenshotUrl && (
+          <div
+            className="preview-tooltip"
+            style={{ left: tooltipPos.x + 20, top: tooltipPos.y + 20 }}
+          >
+            <img src={screenshotUrl} alt="" />
+          </div>
+        )}
+      </article>
+    );
+  }
+}
 
 export const query = graphql`
   query ProjectsQuery {
